a015dacf36a2b38b1c43486bb06a6592cb0d1499
E T <guofoo>
2015-03-12 07:28:19 +0000

    commit exercise content by Live4Code

---
+++ test.js
@@ -0,0 +1,18 @@
+var expect = require('expect.js');
+
+var year = 1984;
+var now  = 2012;
+var age  = now-year    // FILL IN ANSWER HERE
+var output;
+
+function run()
+{
+    output = 'They are either ' + age + ' or ' + (age - 1);
+
+    console.log(output);
+}
+
+run();
+
+expect(age).to.eql(now - year);
+
