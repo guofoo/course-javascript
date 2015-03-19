3ac1d53347df31f9c7b83db4712ccc3f0e4d4682
E T <guofoo>
2015-03-19 07:27:51 +0000

    commit exercise content by Live4Code

--- greaterNum.js
+++ greaterNum.js
@@ -1,5 +1,5 @@
 function greaterNum(num1, num2) {
-    if ( num1 <= num2 ) {            // FILL IN YOUR ANSWER HERE
+    if ( num > num2 ) {            // FILL IN YOUR ANSWER HERE
         return num1;
     } else {
         return num2;
